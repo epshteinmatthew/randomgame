503d727ac86a431519182e11dab532a3faa1e2b6
epshteinmatthew <robotmeatsuit@gmail.com>
2025-05-28 21:56:02 -0700

    create round class and constructor

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 function chose() {
-  alert("wrong!");
+  alert("todo");
+}
+
+function loadRound() {
+  return new game("nothing.png", 2000);
+}
+
+//onload hook
+
+class game {
+  constructor(imageSrc, year) {
+    document.getElementById("image").src = imageSrc;
+    this.year = year;
+    //generate the other two random years
+    this.yearB = 0;
+    this.yearC = 0;
+    //random assignment of buttons
+  }
 }
